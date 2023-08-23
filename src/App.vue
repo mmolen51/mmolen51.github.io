c8bbec8694154d5bd01d5e01f5e1a5a217141135
Melanie Molen <mmolen@purdue.edu>
2023-08-22 20:07:45 -0500

    header

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,52 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import Header from './components/Header.vue'
+</script>
+<script>
+  export default {
+    data() {
+      return {
+        title: 'My test To Do App'
+      }
+    }
+  }
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <Header></Header>
   </header>
 
   <main>
-    <TheWelcome />
+    
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+#app {
+  font-family: Roboto, Helvetica, Arial, sans-serif;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+header {
+  position: fixed;
+  top: 0px;
+  width: 100%;
+  margin: 0 auto;
+  left: 0px;
+  height: 100px;
+  background-color: #05a8aa;
 }
+
+
 
 @media (min-width: 1024px) {
   header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    
+    
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+  
 }
 </style>
